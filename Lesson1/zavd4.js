dfb05d3e61ee2b6e22ad4f65ef5e2450619a7688
Volodya <vovan127@gmail.com>
2023-08-16 17:55:42 +0300

    task 4

--- Lesson1/zavd4.js
+++ Lesson1/zavd4.js
@@ -1,6 +1,7 @@
 let lastName = prompt('Input firstname','Last Name');
 let middleName = prompt('Input firstname','Middle Name');
 let firstName = prompt('Input firstname','First Name');
-console.log(lastName,middleName,firstName);
+let age = prompt('Age','age')
+console.log(lastName,middleName,firstName, age);
 
 
