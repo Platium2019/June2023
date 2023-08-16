d8ad755b774f5772bbdc3888526cb96b8f15de2c
Volodya <vovan127@gmail.com>
2023-08-16 16:32:08 +0300

    task one

---
+++ Lesson1/zavd1.js
@@ -0,0 +1,26 @@
+let a1 ='hello';
+console.log(a1);
+let a2= 'owu';
+console.log(a2);
+let a3 = 'com';
+console.log(a3);
+let a4='ua';
+console.log(a4);
+let a5=1;
+console.log(a5);
+let a6 = 10;
+console.log(a6);
+let a7= -999;
+console.log(a7);
+let a8=123;
+console.log(a8);
+let a9 = 3.14;
+console.log(a9);
+let a10 = 2.7
+console.log(a10);
+let a11 = 16;
+console.log(a11);
+let a12 = true;
+console.log(a12);
+let a13 = false;
+console.log(a13);
