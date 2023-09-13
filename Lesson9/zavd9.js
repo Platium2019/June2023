02e055c76748972bd376b0e75035ca12ce6f3b38
Volodya <vovan127@gmail.com>
2023-09-13 18:43:20 +0300

    Task 9

---
+++ Lesson9/zavd9.js
@@ -0,0 +1,100 @@
+//
+// - створити блок,
+//  - додати йому класи wrap, collapse, alpha, beta
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// - додати цей блок в body.
+// - клонувати його повністю, та додати клон в body.
+let newblock = document.createElement("div");
+newblock.classList.add("wrap", "collapse", "alpha", "beta");
+newblock.style.backgroundColor = "green";
+newblock.style.color = "red";
+newblock.style.fontSize = "20px";
+newblock.textContent = "Block 1";
+document.body.appendChild(newblock);
+const cloneblock = newblock.cloneNode(true);
+cloneblock.textContent = "Clone Block 1 to Block 2";
+document.body.appendChild(cloneblock);
+//
+// - Є масив:
+//     ['Main','Products','About us','Contacts']
+// Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
+const  array = ['Main','Products','About us','Contacts'];
+const ulItem = document.getElementById("menu");
+for (const Item of array) {
+    const item = document.createElement("li");
+    item.textContent = "Block 1";
+      ulItem.appendChild(item);
+
+}
+
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// Завдання робити через цикли.
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+for (const element of coursesAndDurationArray) {
+    const arraydiv = document.createElement("div")
+    let p = document.createElement('p');
+    p.innerText = element.title + ' ' + element.monthDuration;
+    arraydiv.append(p);
+    document.body.append(arraydiv);
+}
+
+
+//
+// =========================
+//
+//     - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+//     Завдання робити через цикли.
+
+for (const element of coursesAndDurationArray) {
+    const arraydiv = document.createElement("div")
+    let p = document.createElement('p');
+    p.innerText = element.title + ' ' + element.monthDuration;
+    arraydiv.append(p);
+    arraydiv.classList.add("item");
+    document.body.append(arraydiv);
+}
+
+// ==========================
+//
+// - є масив simpsons, котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+// Проітерувати його, створиши для кожного об'єкту  масиву <div class='member'> та наповнити його данними з об'єкту.
+//     Якщо людською мовою: під кожного члена родини зробити блок та наповнити його інформацією з цього об'єкту
+//
+// =========================
+
+
+//     Цикл в циклі
+// - Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+//
+// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+// Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фйлом
+// ------------------
+//
+//
