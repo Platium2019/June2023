807590bace0df572dcf6c988f06a52760b96aafb
Volodya <vovan127@gmail.com>
2023-10-06 16:50:26 +0300

    drop

--- Lesson10/dop.js
+++ Lesson10/dop.js
@@ -21,3 +21,20 @@ for (let index = 0; index < title.length; index++) {
 }
 
 https://mixkit.co/free-stock-music/
+
+
+    let newtitle = document.querySelectorAll(".item-grid-card__title");
+let newAutor = document.querySelectorAll(".item-grid-music-preview__author");
+let newMp3 = document.querySelectorAll("audio");
+
+let array = Array.from(newtitle).reduce((accumulator, currentValue, index) => {
+    let data = {
+        title: currentValue.innerText,
+        author: newAutor[index].innerText,
+        mp3: newMp3[index].getAttribute("src")
+    };
+    accumulator.push(data);
+    return accumulator;
+}, []);
+
+console.log(array)
